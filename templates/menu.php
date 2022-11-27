6ebc6d3348b6d5a261f44bbfc22bbb7ad49012ce
xhmyjae <tomatis.mt@gmail.com>
2022-11-27 21:51:07 +0100

    feat: menu component html and css (not finished)

---
+++ templates/menu.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" type="text/css" href="../style/main.css"/>
+<link rel="stylesheet" type="text/css" href="../style/menu.css"/>
+<script src="https://kit.fontawesome.com/74fed0e2b5.js" crossorigin="anonymous"></script>
+
+<div class="menu">
+    <div class="site-title-box">
+        <p id="site-title">SNAPAGRAM</p>
+    </div>
+    <div class="categories">
+        <ul>
+            <li class="category"><span class="list-icon"><i class="fa-regular fa-comment"></i></span>Accueil</li>
+            <li class="category"><span class="list-icon"><i class="fa-regular fa-bell"></i></span>Notifications</li>
+            <li class="category"><span class="list-icon"><i class="fa-solid fa-wand-magic-sparkles"></i></span>Découvrir</li>
+            <li class="category"><span class="list-icon"><i class="fa-regular fa-user"></i></span>Profil</li>
+        </ul>
+    </div>
+    <div class="search-box">
+        <label>
+            <input type="text" class="search-input" placeholder="Recherche...">
+        </label>
+        <button class="search-button"><i class="fas fa-search"></i></button>
+    </div>
+</div>
