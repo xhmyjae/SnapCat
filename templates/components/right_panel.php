bef655c6505723472d459ada04a9f09538fd02b7
xhmyjae <tomatis.mt@gmail.com>
2022-11-27 22:38:50 +0100

    feat: add main comment css to test (need to be put in a component)

--- templates/components/right_panel.php
+++ templates/components/right_panel.php
@@ -4,4 +4,25 @@
 
 <div class="right-panel">
 <!--    switch for content-->
+
+<!--    test d'un commentaire (sera dans un component a part plus tard)-->
+    <div class="main-comment">
+        <div class="avatar-box">
+            <img alt="profile-picture" class="avatar" src="../antaBinome.png">
+        </div>
+        <div class="comment-box">
+            <div class="pseudo-box">
+                <span class="pseudo">Bebou</span>
+            </div>
+            <div class="comment-text-box">
+                <span class="comment">Haha trop nul kheir la loose, du coup margot ce verre ?</span>
+            </div>
+            <div class="stats-box">
+                <span class="up-vote"><i class="fa-solid fa-chevron-up"></i></span>
+                <span class="vote-count">3</span>
+                <span class="down-vote"><i class="fa-solid fa-chevron-down"></i></span>
+            </div>
+        </div>
+    </div>
+
 </div>
