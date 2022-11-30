ca4a9a7ce46404fa62f45407db91a56c9d764e1b
xhmyjae <tomatis.mt@gmail.com>
2022-11-30 09:52:05 +0100

    feat : add post/user/friend base functions

--- src/model/friends.php
+++ src/model/friends.php
@@ -1,1 +1,37 @@
 <?php
+
+namespace Application\Model\Friend;
+
+require_once('src/lib/DatabaseConnection.php');
+
+use Application\Lib\Database\DatabaseConnection;
+use PDO;
+
+class Friend {
+    public int $id;
+    public int $user_id1;
+    public int $user_id2;
+    public bool $accepted;
+}
+
+class FriendRepository {
+    public PDO $databaseConnection;
+
+    public function __construct() {
+        $this->databaseConnection = (new DatabaseConnection())->getConnection();
+    }
+
+    public function getFriends(): array {
+        return $this->databaseConnection->query('SELECT * FROM friends ORDER BY id DESC')->fetchAll(PDO::FETCH_CLASS, Friend::class);
+    }
+
+    public function addFriend(int $id, string $user_id1, string $user_id2, bool $accepted): void {
+        $statement = $this->databaseConnection->prepare('INSERT INTO friends (id, user_id1, user_id2, accepted) VALUES (:id, :user_id1, :user_id2, :accepted)');
+        $statement->execute(compact('id', 'user_id1', 'user_id2', 'accepted'));
+    }
+
+    public function deleteFriend(int $id): void {
+        $statement = $this->databaseConnection->prepare('DELETE FROM friends WHERE id = :id');
+        $statement->execute(compact('id'));
+    }
+}
--- src/model/user.php
+++ src/model/user.php
@@ -1,1 +1,46 @@
 <?php
+
+namespace Application\Model\User;
+
+require_once('src/lib/DatabaseConnection.php');
+
+use Application\Lib\Database\DatabaseConnection;
+use PDO;
+
+class User {
+    public int $id;
+    public string $name;
+    public string $mail;
+    public string $avatar;
+    public string $password;
+    public string $creation_date;
+    public ?string $description;
+}
+
+class UserRepository {
+    public PDO $databaseConnection;
+
+    public function __construct() {
+        $this->databaseConnection = (new DatabaseConnection())->getConnection();
+    }
+
+    public function getUser(string $id): User|false {
+        $statement = $this->databaseConnection->prepare('SELECT * FROM users WHERE id = :id');
+        $statement->execute(compact('id'));
+        return $statement->fetchObject(User::class);
+    }
+
+    public function getUsers(): array {
+        return $this->databaseConnection->query('SELECT * FROM users ORDER BY id DESC')->fetchAll(PDO::FETCH_CLASS, User::class);
+    }
+
+    public function addUser(int $id, string $name, string $mail, string $avatar, string $password, string $creation_date, ?string $description): void {
+        $statement = $this->databaseConnection->prepare('INSERT INTO users (id, name, mail, avatar, password, creation_date, description) VALUES (:id, :name, :mail, :avatar, :password, :creation_date, :description)');
+        $statement->execute(compact('id', 'name', 'mail', 'avatar', 'password', 'creation_date', 'description'));
+    }
+
+    public function deleteUser(int $id): void {
+        $statement = $this->databaseConnection->prepare('DELETE FROM users WHERE id = :id');
+        $statement->execute(compact('id'));
+    }
+}
