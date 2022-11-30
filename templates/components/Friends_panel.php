c20b272ea63889fda1d8e30b4aab670ba8081154
Davy <davy.marthely@ynov.com>
2022-11-30 11:20:57 +0100

    Ajout profil

---
+++ templates/components/Friends_panel.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" type="text/css" href="../../style/main.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/right-panel.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/friends-panel.css"/>
+<script src="https://kit.fontawesome.com/74fed0e2b5.js" crossorigin="anonymous"></script>
+<script src="../../js/friends-panel.js"></script>
+
+<div class="right-panel">
+    <div class="Switch">
+        <div  class="Switch_button" data-switch="friends">Amis</div>
+        <div class="Switch_button" data-switch="comments">Requetes</div>
+        <div class="Switch_button" data-switch="messages">Demandes</div>
+    </div>
+    <div class="Friend-list">
+        <div class="friend" data-friend="1">
+            <div class="friend_avatar">
+                <img src="../antaBinome.png" alt="avatar">
+            </div>
+            <div class="Friend_name">
+                <p>Best binome</p>
+            </div>
+        </div>
+        <div class="friend" data-friend="1">
+            <div class="friend_avatar">
+                <img src="../antaBinome.png" alt="avatar">
+            </div>
+            <div class="Friend_name">
+                <p>Best binome</p>
+            </div>
+            <div class="Accept_button">
+                <i class="fas fa-check"></i>
+                <i class="fas fa-xmark"></i>
+        </div>
+    </div>
+
+</div>
---
+++ templates/components/right_panel.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" type="text/css" href="../../style/main.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/right-panel.css"/>
+<script src="https://kit.fontawesome.com/74fed0e2b5.js" crossorigin="anonymous"></script>
+
+<div class="right-panel">
+<!--    switch for content-->
+
+<!--    test d'un commentaire (sera dans un component a part plus tard)-->
+    <div class="main-comment">
+        <div class="avatar-box">
+            <img alt="profile-picture" class="avatar" src="../Img/antaBinome.png">
+        </div>
+        <div class="comment-box">
+            <div class="pseudo-box">
+                <span class="pseudo">Bebou</span>
+            </div>
+            <div class="comment-text-box">
+                <span class="comment">Haha trop nul kheir la loose, du coup margot ce verre ?</span>
+            </div>
+            <div class="stats-box">
+                <span class="up-vote"><i class="fa-solid fa-chevron-up"></i></span>
+                <span class="vote-count">3</span>
+                <span class="down-vote"><i class="fa-solid fa-chevron-down"></i></span>
+            </div>
+        </div>
+    </div>
+
+</div>
