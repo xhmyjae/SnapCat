42d08c28d9f6d3c44838e622810608ee4ebf9846
Davy <davy.marthely@ynov.com>
2022-11-30 15:07:40 +0100

    Ajout profil

---
+++ client/templates/components/profil.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" type="text/css" href="../../style/main.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/profil.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/menu.css"/>
+<link rel="stylesheet" type="text/css" href="../../style/right-panel.css"/>
+<script src="https://kit.fontawesome.com/74fed0e2b5.js" crossorigin="anonymous"></script>
+
+<div class="profil-panel">
+    <div class="top-profil">
+        <div class="banner">
+            <img src="../Img/GodOfWar3_Hero.jpg" alt="banner profil">
+        </div>
+        <div class="profil-picture">
+            <img src="../Img/Ezio.jpg" alt="picture profil">
+            <p>Ezio Auditore</p>
+            <p class="bio">dddddddddddddddddddddddddddddddddddddddddddddddddd dddddddddd dddddddddd dddddddddd dddddddddd dddddddddd dddddddddd dddddddddd dddddddddddddddddddd</p>
+        </div>
+        <div class="friends">
+            <p>Amis</p>
+        </div>
+    </div>
+    <div class="publications">
+        <div class="container-post"></div>
+        <div class="container-post"></div>
+        <div class="container-post"></div>
+        <div class="container-post"></div>
+        <div class="container-post"></div>
+        <div class="container-post"></div>
+    </div>
+</div>
+
+<?php
