0ff259365970e10cd7aef4098493ffb0fb2dfc67
xhmyjae <tomatis.mt@gmail.com>
2022-12-12 12:40:00 +0100

    feat: change model tag to dialog tag

--- client/scripts/popup-post.js
+++ client/scripts/popup-post.js
@@ -1,7 +1,7 @@
 const modals = document.querySelectorAll('.modal-post');
 const btn = document.querySelectorAll('.post-profile-hover');
 const closeBtn = document.querySelectorAll('.modal-close');
-console.log(closeBtn);
+let profilePanel = document.querySelector('.profil-panel');
 
 const keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
@@ -16,20 +16,23 @@ function preventDefaultForScrollKeys(e) {
     }
 }
 
-// Make modal appear by adding modal-display class on btn click
 btn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         let clicked = e.target;
-        let modal = clicked.parentElement.lastElementChild;
-        modal.classList.add('modal-display');
+        let modal = clicked.parentElement.querySelector('.modal-background');
+        if (typeof modal.showModal === "function") {
+            modal.showModal();
+        }
     });
 });
 
 closeBtn.forEach((closeBtn) => {
     closeBtn.addEventListener('click', (e) => {
         let clicked = e.target;
-        let modal = clicked.parentElement.parentElement.parentElement;
+        let modal = clicked.parentElement.parentElement.parentElement.parentElement;
         console.log(modal);
-        modal.classList.remove('modal-display');
+        modal.close();
     });
 });
+
+
--- client/templates/components/pop-up-post.php
+++ client/templates/components/pop-up-post.php
@@ -20,52 +20,60 @@ if ($posts !== null) {
             <i class="fa-solid fa-comment"></i>
         </div>
 
-        <div class="modal-post">
-            <div class="modal-post-content">
-                <p class="modal-post-date"><?= $post['creation_date'] ?></p>
-                <p class="modal-post-content-text"><?= $post['message'] ?></p>
-            </div>
-            <div class="modal-information">
-                <div class="modal-close"><i class="fa-solid fa-xmark"></i></div>
-                <div class="modal-reactions">
-                    <div class="modal-reaction">
-                        <a href="" class="modal-reaction-icon"><i class="fa-solid fa-thumbs-up"></i><p class="modal-reaction-number">222</p></a>
-                    </div>
-                    <div class="modal-reaction">
-                        <a href="" class="modal-reaction-icon"><i class="fa-solid fa-thumbs-down"></i><p class="modal-reaction-number">222</p></a>
-                    </div>
-                    <div class="modal-reaction">
-                        <a href="" class="modal-reaction-icon"><i class="fa-solid fa-heart"></i><p class="modal-reaction-number">222</p></a>
-                    </div>
-                    <div class="modal-reaction">
-                        <a href="" class="modal-reaction-icon"><i class="fa-solid fa-star"></i><p class="modal-reaction-number">222</p></a>
-                    </div>
-                    <div class="modal-reaction">
-                        <a href="" class="modal-reaction-icon"><i class="fa-solid fa-star"></i><p class="modal-reaction-number">222</p></a>
-                    </div>
+        <dialog class="modal-background">
+            <div class="modal-post">
+                <div class="modal-post-content">
+                    <p class="modal-post-date"><?= $post['creation_date'] ?> <?= $post['emotion'] == 1 ? ' 😄' : ($post['emotion'] == 2 ? ' 😔' : ($post['emotion'] == 3 ? ' 🤔' : ' 😭')) ?></p>
+                    <p class="modal-post-content-text"><?= $post['message'] ?></p>
                 </div>
-                <div class="modal-comments">
-                    <?php foreach ($post_comments as $comment) {
-                    $comment_user = (new GetUser())->execute($comment['user_id']); ?>
-                    <div class="modal-comment">
-                        <img class="modal-comment-avatar" src="../img/<?= $comment_user->avatar ?>.png" alt="comment avatar">
-                        <div class="modal-comment-content">
-                            <div class="modal-comment-information">
-                                <p class="modal-comment-name"><?= $comment_user->name ?></p>
-                                <p class="modal-comment-date"><?= $comment['creation_date'] ?></p>
+                <div class="modal-information">
+                    <div class="modal-close">
+                        <form action="/delete_post" method="POST">
+                            <input type="hidden" name="post_id" value="<?= $post['id'] ?>">
+                            <button type="submit" class="delete-button form-btn" value="delete_post"><i class="fa-solid fa-trash-can"></i></button>
+                        </form>
+                        <i class="fa-solid fa-xmark"></i>
+                    </div>
+                    <div class="modal-reactions">
+                        <div class="modal-reaction">
+                            <a href="" class="modal-reaction-icon"><i class="fa-solid fa-thumbs-up reaction-icon"></i><p class="modal-reaction-number">222</p></a>
+                        </div>
+                        <div class="modal-reaction">
+                            <a href="" class="modal-reaction-icon"><i class="fa-solid fa-thumbs-down reaction-icon"></i><p class="modal-reaction-number">222</p></a>
+                        </div>
+                        <div class="modal-reaction">
+                            <a href="" class="modal-reaction-icon"><i class="fa-solid fa-heart reaction-icon"></i><p class="modal-reaction-number">222</p></a>
+                        </div>
+                        <div class="modal-reaction">
+                            <a href="" class="modal-reaction-icon"><i class="fa-solid fa-star reaction-icon"></i><p class="modal-reaction-number">222</p></a>
+                        </div>
+                        <div class="modal-reaction">
+                            <a href="" class="modal-reaction-icon"><i class="fa-solid fa-star reaction-icon"></i><p class="modal-reaction-number">222</p></a>
+                        </div>
+                    </div>
+                    <div class="modal-comments">
+                        <?php foreach ($post_comments as $comment) {
+                            $comment_user = (new GetUser())->execute($comment['user_id']); ?>
+                            <div class="modal-comment">
+                                <img class="modal-comment-avatar" src="../img/<?= $comment_user->avatar ?>.png" alt="comment avatar">
+                                <div class="modal-comment-content">
+                                    <div class="modal-comment-information">
+                                        <p class="modal-comment-name"><?= $comment_user->name ?></p>
+                                        <p class="modal-comment-date"><?= $comment['creation_date'] ?></p>
+                                    </div>
+                                    <div class="modal-comment-text"><?= $comment['message'] ?></div>
+                                </div>
+                                <div class="modal-comment-votes">
+                                    <i class="fa-solid fa-chevron-up"></i>
+                                    <p class="modal-comment-votes-total"><?= $comment['vote'] ?></p>
+                                    <i class="fa-solid fa-chevron-down"></i>
+                                </div>
                             </div>
-                            <div class="modal-comment-text"><?= $comment['message'] ?></div>
-                        </div>
-                        <div class="modal-comment-votes">
-                            <i class="fa-solid fa-chevron-up"></i>
-                            <p class="modal-comment-votes-total"><?= $comment['vote'] ?></p>
-                            <i class="fa-solid fa-chevron-down"></i>
-                        </div>
+                        <?php } ?>
                     </div>
-                    <?php } ?>
                 </div>
             </div>
-        </div>
+        </dialog>
     </div>
     <?php } ?>
 <?php } else {
